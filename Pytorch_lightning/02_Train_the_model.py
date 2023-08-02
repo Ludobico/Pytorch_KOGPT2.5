330c1c2fe725b93f540d3101969ea7169653f54d
Ludobico <aqs450@gmail.com>
2023-08-02 11:43:58 +0900

    pytorch lightning tutorial

--- Pytorch_lightning/02_Train_the_model.py
+++ Pytorch_lightning/02_Train_the_model.py
@@ -1,4 +1,6 @@
 import os
+from typing import Any
+from pytorch_lightning.utilities.types import STEP_OUTPUT
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -32,3 +34,22 @@ class Decoder(nn.Module):
 
     def forward(self, x):
         return self.l1(x)
+
+
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
